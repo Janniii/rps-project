ea0d0d885de81c04cd82fe29014a2c1d19fe2935
Janni <36205116+Janniii@users.noreply.github.com>
2022-05-31 20:24:05 +0200

    Add computerPlay playRound and game functions to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 let rpsOptions = ["Rock", "Paper", "Scissors"]
+const playerSelection = "Scissors"
+//const computerSelection = computerPlay()
+const playerSelectionReal = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase()
 
 
 
@@ -10,6 +13,86 @@ function computerPlay() {
 
 
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        // console.log("DRAW GAME!");
+        return 2
+    } 
+      else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+          // console.log("You win! Rock beats Scissors");
+          return 1
+      }
+
+      else if (playerSelection == "Rock" && computerSelection == "Paper") {
+          // console.log("You lose! Paper beats Rock");
+          return 0
+      }
 
 
-console.log(computerPlay());
+      else if (playerSelection == "Paper" && computerSelection == "Rock") {
+          // console.log("You win! Paper beats Rock");
+          return 1
+      }
+
+      else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+          // console.log("You lose! Scissors beats Paper");
+          return 0
+      }
+
+      else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+          // console.log("You win! Scissors beats Paper");
+          return 1
+      }
+
+      else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+          // console.log("You lose! Rock beats Scissors");
+          return 0
+      }
+
+}
+
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay()
+        let round = playRound(playerSelectionReal, computerSelection)
+
+        if (round == 1) {
+            playerScore += 1
+        }
+
+        else if (round == 0) {
+            computerScore += 1
+        }
+
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You won! Overall Score: ", playerScore, "to", computerScore);
+    }
+
+    else if (playerScore < computerScore) {
+        console.log("You lost! Overall Score: ", playerScore, "to", computerScore);
+    }
+
+    else {
+        console.log("Draw Game! Overall Score: ", playerScore, "to", computerScore);
+    }
+}
+
+
+game()
+
+
+
+
+
+
+
+//console.log(playerSelectionReal, computerSelection);
+//console.log(playRound(playerSelectionReal, computerSelection))
+
+
